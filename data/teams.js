1ba9cfc6fe7f10c285faa4009a742de021c4eec2
Jamie Brown <jamiebrown201@hotmail.com>
2020-02-17 14:15:09 +0000

    format

--- data/teams.js
+++ data/teams.js
@@ -1,18 +1,18 @@
 module.exports = {
-	TEAMS: (() => {
-		return [ 
-			{
-				name: 'Labs Team',
-				channel: process.env.LABS_CHANNEL
-			},
-			{
-				name: 'Product Team',
-				channel: process.env.PRODUCT_ORG_CHANNEL
-			},
-			{
-				name: 'Test Team',
-				channel: process.env.TEST_CHANNEL
-			}
-		];
-	})()
-}
+  TEAMS: (() => {
+    return [
+      {
+        name: "Labs Team",
+        channel: process.env.LABS_CHANNEL
+      },
+      {
+        name: "Product Team",
+        channel: process.env.PRODUCT_ORG_CHANNEL
+      },
+      {
+        name: "Test Team",
+        channel: process.env.TEST_CHANNEL
+      }
+    ];
+  })()
+};
--- data/types.js
+++ data/types.js
@@ -1,36 +1,47 @@
 module.exports = {
-	AWAY_TYPES: (() => {
-		return [
-			{
-				keywords: ['ill', 'sick'],
-				emoji: [':ill:', ':face_with_head_bandage:'],
-				type: 'ill'
-			},
-			{
-				keywords: ['ooo', 'office', 'out', 'busy', 'conference', 'training'],
-				emoji: [':no_entry_sign:'],
-				type: 'ooo'
-			},
-			{
-				keywords: ['home', 'wfh', 'working', 'work'],
-				emoji: [':house:', ':house_with_garden:', ':computer:'],
-				type: 'wfh'
-			},
-			{
-				keywords: ['birthday', 'cake', ':cake:'],
-				emoji: [':birthday:', ':cake:', ':cupcake:'],
-				type: 'cake'
-			},
-			{
-				keywords: ['mat', 'maternity', 'pat', 'paternity', 'parental'],
-				emoji: [':baby:', ':baby_bottle:', ':pregnant_woman:'],
-				type: 'parent'
-			},
-			{
-				keywords: ['holiday', 'vacation', 'vacationing', 'annual', 'leave', 'al', 'a/l', 'travelling', 'travel', 'traveling'],
-				emoji: [':palm_tree:', ':airplane:', ':desert_island', ':sunny:'],
-				type: 'holiday'
-			}
-		];
-	})()
-}
+  AWAY_TYPES: (() => {
+    return [
+      {
+        keywords: ["ill", "sick"],
+        emoji: [":ill:", ":face_with_head_bandage:"],
+        type: "ill"
+      },
+      {
+        keywords: ["ooo", "office", "out", "busy", "conference", "training"],
+        emoji: [":no_entry_sign:"],
+        type: "ooo"
+      },
+      {
+        keywords: ["home", "wfh", "working", "work"],
+        emoji: [":house:", ":house_with_garden:", ":computer:"],
+        type: "wfh"
+      },
+      {
+        keywords: ["birthday", "cake", ":cake:"],
+        emoji: [":birthday:", ":cake:", ":cupcake:"],
+        type: "cake"
+      },
+      {
+        keywords: ["mat", "maternity", "pat", "paternity", "parental"],
+        emoji: [":baby:", ":baby_bottle:", ":pregnant_woman:"],
+        type: "parent"
+      },
+      {
+        keywords: [
+          "holiday",
+          "vacation",
+          "vacationing",
+          "annual",
+          "leave",
+          "al",
+          "a/l",
+          "travelling",
+          "travel",
+          "traveling"
+        ],
+        emoji: [":palm_tree:", ":airplane:", ":desert_island", ":sunny:"],
+        type: "holiday"
+      }
+    ];
+  })()
+};
